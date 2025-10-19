2f2dd9fafe2381266f3da359f1d202fb144912da
Achmad Hasan Bashori <bashorihasan2@gmail.com>
2025-10-19 15:33:17 +0700

    fix error

--- src/App.vue
+++ src/App.vue
@@ -45,14 +45,6 @@ export default {
   box-sizing: border-box;
 }
 
-<style>
-/* Global Styles */
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
 body {
   font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
   background: #ffffff;
